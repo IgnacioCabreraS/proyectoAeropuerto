349cb276782e2bb104761f93eabdff65ab14d852
marr109 <88809862+marr109@users.noreply.github.com>
2021-11-21 17:10:46 -0300

    Menús main
    Se modificaron y crearon opciones del proyecto.

--- main.c
+++ main.c
@@ -25,34 +25,86 @@ typedef struct{
 
 int lower_than_double(void * , void* );
 char*get_csv_field (char * tmp, int k);
-TreeMap * importar(TreeMap * mapaVuelo);
-void comprarPasaje(TreeMap * mapaVuelo, List* listPasajes);
+TreeMap* importar(TreeMap * mapaVuelo);
+void* modoPasajero(TreeMap* mapaVuelo, List* listaPasajes);
+void* modoAdministrador(TreeMap* mapaVuelo, List* listaPasajes);
+void* comprarPasaje(TreeMap* mapaVuelo, List* listaPasajes);
+void* busquedaVuelos(TreeMap* mapaVuelo);
+void* estadoVuelos(List* listaPasajes);
+void* revisarPasaje(List* listaPasajes);
+void* ofertasDisponibles(TreeMap* mapaVuelo);
+void* modificarVuelos(TreeMap* mapaVuelo);
+
+
 
 int main (){
 
     TreeMap* mapaVuelo = createTreeMap(lower_than_double);
     List* listaPasajes = createList();
     importar(mapaVuelo);
-    /*
+    
     int opcion=1;
     while(opcion!=0){
-        printf("1. Comprar pasaje.\n");
+        printf("1. Modo pasajero.\n");
+        printf("2. Modo administrador.\n");
         scanf("%d",&opcion);
         switch(opcion){
-            
+            case 1:modoPasajero(mapaVuelo,listaPasajes);break; //  pasar opcion para verificar si se sale del switch
+            case 2:modoAdministrador(mapaVuelo, listaPasajes);break;
         }
     }
-    */
-    
     return 0;
 }
+
+void* modoPasajero (TreeMap* mapaVuelo,List* listaPasajes){
+
+    int opcion = 1;
+    while(opcion != 0){
+        printf("1. Comprar pasaje.\n");
+        printf("2. Busqueda de vuelos.\n");
+        printf("3. Estado vuelos.\n");
+        printf("4. Revisar pasajes comprados.\n");
+        printf("5. Ofertas disponibles.\n");
+        printf("6. Pasar a modo administrador.\n");
+        
+        scanf("%d", &opcion);
+        switch (opcion){
+            case 1: comprarPasaje(mapaVuelo,listaPasajes);break;
+            case 2: busquedaVuelos(mapaVuelo);break;
+            case 3: estadoVuelos(listaPasajes);break;
+            case 4: revisarPasaje(listaPasajes);break;
+            case 5: ofertasDisponibles(mapaVuelo);break;
+            case 6: modoAdministrador(mapaVuelo,listaPasajes);break;
+            default: break;
+        }
+    }
+    
+}
+
+void* modoAdministrador (TreeMap* mapaVuelo,List* listaPasajes){
+
+    int opcion = 1;
+    while(opcion != 0){
+        printf("1. Modificar informacion vuelos.\n");
+        printf("2. Pasar modo pasajero.\n");
+        
+        scanf("%d", &opcion);
+        switch (opcion){
+            case 1:modificarVuelos(mapaVuelo);break;
+            case 2:modoPasajero(mapaVuelo,listaPasajes);break;
+            default: break;
+        }
+    }
+}
+
+
 
 int lower_than_double(void * key1, void * key2) {
     if(*(double*)key1 > *(double*)key2) return 1;
     return 0;
 }
 
-char*get_csv_field (char * tmp, int k){
+char * get_csv_field (char * tmp, int k){
     int open_mark = 0;
     char* ret=(char*) malloc(100*sizeof(char));
     int ini_i=0, i=0;
@@ -141,3 +193,26 @@ TreeMap * importar(TreeMap * mapaVuelos){
     } 
 }
 
+void * comprarPasaje(TreeMap *mapaVuelo, List *listaPasajes){
+    
+}
+
+void * busquedaVuelos(TreeMap *mapaVuelo){
+    
+}
+
+void * estadoVuelos(List *listaPasajes){
+    
+}
+
+void * revisarPasaje(List *listaPasajes){
+    
+}
+
+void * ofertasDisponibles(TreeMap *mapaVuelo){
+    
+}
+
+void * modificarVuelos(TreeMap *mapaVuelo){
+    
+}
